363ca24dda462f5b15da6bc1e7cbc65750e83ee6
Z <helloeaton@gmail.com>
2025-07-22 12:17:32 +1000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
 
 using PointsBet_Backend_Online_Code_Test;
 
@@ -6,12 +6,12 @@ class Program
 {
     static void Main()
     {
-        string[] items = { "apple", "banana", "cherry" };
-        string quote = "\"";
+        string[] items = { "a", "b", "c" };
 
-        StringFormatter formatter = new StringFormatter(items, quote);
 
-        string result = formatter.ToCommaSeparatedList();
+        StringFormatter formatter = new StringFormatter(items);
+
+        string result = formatter.ToCommaSeparatedListWithQuote();
         Console.WriteLine(result);
 
     }
